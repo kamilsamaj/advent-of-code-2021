b4b9d8da453c3ba1d1c2d184d1879ff47bc799dd
kamilsamaj <kamilsamaj@gmail.com>
2021-12-15 23:54:28 +0100

    day 12 task 2

--- cmd/12/main.go
+++ cmd/12/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"github.com/kamilsamaj/advent-of-code-2021/internal"
 	"log"
+	"regexp"
 	"strings"
 	"unicode"
 )
 
 type graph struct {
 	nodes          map[string][]string // map node names with its neighbors
-	visitedPaths   map[string]bool
+	visitedPaths   map[string]string   // what paths were visited and if any small letter is already twice there
 	finalizedPaths map[string]bool
 }
 
@@ -25,7 +26,7 @@ func containsString(slice []string, str string) bool {
 
 func (g *graph) load(lines []string) {
 	g.nodes = make(map[string][]string)
-	g.visitedPaths = make(map[string]bool)
+	g.visitedPaths = make(map[string]string)
 	g.finalizedPaths = make(map[string]bool)
 	for _, line := range lines {
 		n := strings.Split(line, "-")
@@ -63,8 +64,36 @@ func (g *graph) visitNeighbors(node string, previouslyVisited string) {
 		} else {
 			if (unicode.IsLower(rune(n[0])) && !strings.Contains(previouslyVisited, fmt.Sprint(",", n, ","))) ||
 				unicode.IsUpper(rune(n[0])) {
-				g.visitedPaths[fmt.Sprint(previouslyVisited, n, ",")] = true
+				g.visitedPaths[fmt.Sprint(previouslyVisited, n, ",")] = ""
 				g.visitNeighbors(n, fmt.Sprint(previouslyVisited, n, ","))
+			}
+		}
+	}
+}
+
+func (g *graph) visitNeighbors2(node string, previouslyVisited string) {
+	// for each neighbor
+	for _, n := range (*g).nodes[node] {
+		if n == "start" {
+			continue
+		} else if n == "end" {
+			finPath := fmt.Sprint(previouslyVisited, n)
+			if _, ok := g.finalizedPaths[finPath]; !ok {
+				g.finalizedPaths[finPath] = true
+			}
+		} else {
+			r := regexp.MustCompile(fmt.Sprint(",", n, ","))
+			if (unicode.IsLower(rune(n[0])) && g.visitedPaths[previouslyVisited] == "" && len(r.FindAllStringIndex(previouslyVisited, -1)) <= 1) ||
+				(unicode.IsLower(rune(n[0])) && g.visitedPaths[previouslyVisited] != "" && len(r.FindAllStringIndex(previouslyVisited, -1)) == 0) ||
+				unicode.IsUpper(rune(n[0])) {
+
+				newPath := fmt.Sprint(previouslyVisited, n, ",")
+				if unicode.IsLower(rune(n[0])) && g.visitedPaths[previouslyVisited] == "" && len(r.FindAllStringIndex(newPath, -1)) == 2 {
+					g.visitedPaths[newPath] = n
+				} else {
+					g.visitedPaths[newPath] = g.visitedPaths[previouslyVisited]
+				}
+				g.visitNeighbors2(n, newPath)
 			}
 		}
 	}
@@ -73,8 +102,16 @@ func (g *graph) visitNeighbors(node string, previouslyVisited string) {
 func task1(lines []string) int {
 	g := graph{}
 	g.load(lines)
-	g.visitedPaths["start,"] = true
+	g.visitedPaths["start,"] = ""
 	g.visitNeighbors("start", "start,")
+	return len(g.finalizedPaths)
+}
+
+func task2(lines []string) int {
+	g := graph{}
+	g.load(lines)
+	g.visitedPaths["start,"] = ""
+	g.visitNeighbors2("start", "start,")
 	return len(g.finalizedPaths)
 }
 
@@ -87,5 +124,6 @@ func main() {
 
 	// be careful about the linebreak in the last number
 	lines := strings.Split(strings.Trim(string(input), "\n"), "\n")
-	fmt.Println("Task 1: Finalized paths:", task1(lines))
+	fmt.Println("Task 1: Finalized paths (small only once):", task1(lines))
+	fmt.Println("Task 1: Finalized paths (small max twice):", task2(lines))
 }
--- cmd/12/main_test.go
+++ cmd/12/main_test.go
@@ -37,6 +37,15 @@ pj-fs
 start-RW
 `
 
+const input3 = `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+`
+
 func TestTask1a(t *testing.T) {
 	lines := strings.Split(strings.Trim(string(input), "\n"), "\n")
 	if res := task1(lines); res != 19 {
@@ -48,5 +57,26 @@ func TestTask1b(t *testing.T) {
 	lines := strings.Split(strings.Trim(string(input2), "\n"), "\n")
 	if res := task1(lines); res != 226 {
 		t.Errorf("expected: 226 finalized paths, got: %d", res)
+	}
+}
+
+func TestTask2a(t *testing.T) {
+	lines := strings.Split(strings.Trim(string(input), "\n"), "\n")
+	if res := task2(lines); res != 103 {
+		t.Errorf("expected: 103 finalized paths, got: %d", res)
+	}
+}
+
+func TestTask2b(t *testing.T) {
+	lines := strings.Split(strings.Trim(string(input2), "\n"), "\n")
+	if res := task2(lines); res != 3509 {
+		t.Errorf("expected: 3509 finalized paths, got: %d", res)
+	}
+}
+
+func TestTask2c(t *testing.T) {
+	lines := strings.Split(strings.Trim(string(input3), "\n"), "\n")
+	if res := task2(lines); res != 36 {
+		t.Errorf("expected: 36 finalized paths, got: %d", res)
 	}
 }
